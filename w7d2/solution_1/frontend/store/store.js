5cc3f5b3ec4fa6d32c436e988d32a799079fc3ac
Andrew Jiang <andrew@Andrews-MacBook-Pro.local>
2017-03-06 20:08:26 -0800

    Complete

--- w7d2/solution_1/frontend/store/store.js
+++ w7d2/solution_1/frontend/store/store.js
@@ -1,12 +1,32 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import RootReducer from '../reducers/root_reducer';
 
+const addLoggingToDispatch = store => next => action => {
+      console.log(store.getState());
+      console.log(action);
+      let result = next(action);
+      console.log(store.getState());
+      return result;
+};
+
+const applyMiddlewares = (store, ...middlewares) => {
+  let dispatch = store.dispatch;
+  middlewares.forEach( middleware => {
+    dispatch = middleware(store)(dispatch);
+  });
+  return Object.assign({}, store, { dispatch });
+};
+
 const configureStore = (preloadedState = {}) => {
-  const store = createStore(RootReducer, preloadedState);
+  const store = createStore(
+    RootReducer,
+    preloadedState,
+    applyMiddleware(addLoggingToDispatch)
+  );
   store.subscribe(() => {
     localStorage.state = JSON.stringify(store.getState());
   });
   return store;
-}
+};
 
 export default configureStore;
